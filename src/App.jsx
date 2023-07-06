b5655a3e4a90cf42f2bd37a3618cebc126104f6d
Pedro Alexander Garcia A <pedroalexandergarciaa@pedros-imac.myfiosgateway.com>
2023-07-06 19:41:39 -0400

    working progress

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,7 @@
 // Write your Color component here
+const Color = ({ color }) => {
+  return <div className={color}></div>;
+};
 
 const App = () => {
   return (
@@ -7,7 +10,12 @@ const App = () => {
         <div>Currently selected: </div>
         <div className="red">red</div>
       </div>
-      <div id="colors-list">{/* colors go here */}</div>
+      <div id="colors-list">
+        <Color color="orange" />
+        <Color color="violet" />
+        <Color color="red" />
+      </div>
+      ;
     </div>
   );
 };
